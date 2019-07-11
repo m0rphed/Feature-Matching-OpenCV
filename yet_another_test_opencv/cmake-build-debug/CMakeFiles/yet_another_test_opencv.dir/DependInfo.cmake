c91c49a71d19264566e28cceffd33ede0328687c
Victor <victorKhovTko@yandex.ru>
2019-07-11 16:29:50 +0300

    Demo: OpenCV in Clion

---
+++ yet_another_test_opencv/cmake-build-debug/CMakeFiles/yet_another_test_opencv.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vic/my_proj/comp_vis_notes/OpenCvInClion/yet_another_test_opencv/main.cpp" "/home/vic/my_proj/comp_vis_notes/OpenCvInClion/yet_another_test_opencv/cmake-build-debug/CMakeFiles/yet_another_test_opencv.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/anaconda/include"
+  "/usr/local/anaconda/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ yet_another_test_opencv/cmake-build-debug/CMakeFiles/yet_another_test_opencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yet_another_test_opencv.dir/main.cpp.o"
+  "yet_another_test_opencv.pdb"
+  "yet_another_test_opencv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yet_another_test_opencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
